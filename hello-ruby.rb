ca7e216ca1f96da4be4418141f0ee836fc99c6a2
mv <mvatinel38@gmail.com>
2020-06-05 19:05:38 +0200

    ajout fichiers solos

---
+++ hello-ruby.rb
@@ -0,0 +1,21 @@
+puts "Hello"
+
+Kaime = "plantes"
+Léo = 7/20
+Naru = ["feu", "essence_cristal", "Kyoku", "Ombre", "faucheuse", "omniscience"]
+Lilou = [["force", "forme_lézard", "forme_géante"], ["Zoé", "Léo"], "amis"]
+
+Class Naru
+    def colère
+        puts "Son pouvoir se dégage de son corps"
+    end
+    def joie
+        puts "Ouai ! Grave ! On y va !!"
+    end
+    def tristesse
+        puts "Désolé, mais là j'ai besoin d'être seul"
+    end
+    def courage
+        puts "Jamais, tu m'entends ?! Jamais je ne baisserais les bras ! Alors, tu vas arrêter de me juger car.. JE VAIS TE FAIRE TAIRE !!!"
+    end
+end
